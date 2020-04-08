1ed4dee907948305028ee61e203b40d185686855
Muhamad Raihan Fadilah <63182774+RaihanEradicator@users.noreply.github.com>
2020-04-08 17:32:29 +0700

    Contoh Program sederhana KataKunci
    KataKunci Admin = 4576
    KataKunci Pengunjung = 4456

---
+++ Password.py
@@ -0,0 +1,11 @@
+print("Hai Selamat Datang")
+x = (int(input("Masukkan sandi :  ")))
+
+if (x == 4576):
+    print ("Selamat datang kembali Admin")
+    print("")
+    print ("apa yang ingin anda lakukan ?")
+elif (x == 4456):
+    print ("Selamat datang pengunjung")
+else:
+    print ("Maaf sandi yang anda masukkan salah !!! ")
